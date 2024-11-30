423824e84715b505d74f03bd63d2b05bb6dde031
VietHoang117 <144225909+VietHoang117@users.noreply.github.com>
2024-11-30 21:41:53 +0700

    Update JustificationController.php

--- app/Http/Controllers/JustificationController.php
+++ app/Http/Controllers/JustificationController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SettingEmail;
+use App\Enums\AttendanceStatusEnum;
 
 class JustificationController extends Controller
 {
